9b8dc0bccca0c5e630bdb7c68f70f4b87ae2ae18
Alexandre Gressier <alexandre@gressier.dev>
2021-11-29 11:11:11 +0100

    Remove useless blank lines

--- counter-shared/src/commonMain/kotlin/dev/gressier/counter/page/Page.kt
+++ counter-shared/src/commonMain/kotlin/dev/gressier/counter/page/Page.kt
@@ -3,14 +3,12 @@ package dev.gressier.counter.page
 import dev.gressier.counter.components.*
 
 interface Page {
-
     val counter: Counter
     val itemList: ItemList
     val editor: Editor
 }
 
 class PageComponent: Page {
-
     override val counter: Counter = CounterComponent()
     override val itemList: ItemList = ItemListComponent()
     override val editor: Editor = EditorComponent()
